ce813c4a28411f3cc15b92ebedfb3105e461e17e
MaGaroo <amoomajid99@gmail.com>
2019-06-12 14:15:33 +0430

    Update membership definition

--- src/models/membership.go
+++ src/models/membership.go
@@ -1,7 +1,11 @@
 package models
 
-import "github.com/c-beel/userman/src/pkg/api/v1"
+import "github.com/jinzhu/gorm"
 
 type Membership struct {
-	v1.Membership
+	gorm.Model
+	User  User  `gorm:"foreignkey:UID"`
+	UID   int64 `gorm:"column:uid"`
+	Group Group `gorm:"foreignkey:GID"`
+	GID   int64 `gorm:"column:gid"`
 }
