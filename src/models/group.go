66343416cf39cd42f0cd886d560f2484ee8a9d78
MaGaroo <amoomajid99@gmail.com>
2019-06-12 13:20:38 +0430

    Add date fields to user and group

--- src/models/group.go
+++ src/models/group.go
@@ -1,7 +1,11 @@
 package models
 
-import "github.com/c-beel/userman/src/pkg/api/v1"
+import (
+	"github.com/c-beel/userman/src/pkg/api/v1"
+	"github.com/jinzhu/gorm"
+)
 
 type Group struct {
+	gorm.Model
 	v1.Group
 }
