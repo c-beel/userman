7f3ddff170d4552c0e911c7299e73dcd52ed3bd2
MaGaroo <amoomajid99@gmail.com>
2019-09-13 11:05:07 +0430

    Remove uid from everywhere

--- src/pkg/service/v1/user-basic-methods.go
+++ src/pkg/service/v1/user-basic-methods.go
@@ -11,14 +11,13 @@ import (
 func (server UsermanServer) CreateUser(ctx context.Context, req *v1.CreateUserRequest) (*v1.CreateUserResponse, error) {
 	var user models.User
 
-	userGrpcToModels(req.User, &user, false)
-	user.ID = 0
+	userGrpcToModels(req.User, &user)
 
 	if err := server.DB.Create(&user).Error; err != nil {
 		return nil, status.Errorf(codes.Unknown, "failed to create user with error %v.", err)
 	}
 	var newUser v1.User
-	userModelsToGrpc(&user, &newUser, true)
+	userModelsToGrpc(&user, &newUser)
 	return &v1.CreateUserResponse{
 		User: &newUser,
 	}, nil
@@ -26,11 +25,11 @@ func (server UsermanServer) CreateUser(ctx context.Context, req *v1.CreateUserRe
 
 func (server UsermanServer) ReadUser(ctx context.Context, req *v1.ReadUserRequest) (*v1.ReadUserResponse, error) {
 	var user models.User
-	if err := server.DB.First(&user, req.Uid).Error; err != nil {
-		return nil, status.Errorf(codes.NotFound, "failed to get user with this id(%d) with error %v", req.Uid, err)
+	if err := server.DB.First(&user, req.Username).Error; err != nil {
+		return nil, status.Errorf(codes.NotFound, "failed to get user with this username(%s) with error %v", req.Username, err)
 	}
 	var newUser v1.User
-	userModelsToGrpc(&user, &newUser, true)
+	userModelsToGrpc(&user, &newUser)
 	return &v1.ReadUserResponse{
 		User: &newUser,
 	}, nil
@@ -39,17 +38,17 @@ func (server UsermanServer) ReadUser(ctx context.Context, req *v1.ReadUserReques
 func (server UsermanServer) UpdateUser(ctx context.Context, req *v1.UpdateUserRequest) (*v1.UpdateUserResponse, error) {
 	var user models.User
 
-	if err := server.DB.First(&user, req.User.Id).Error; err != nil {
-		return nil, status.Errorf(codes.NotFound, "failed to get user with this id(%d) with error %v", req.User.Id, err)
+	if err := server.DB.First(&user, req.User.Username).Error; err != nil {
+		return nil, status.Errorf(codes.NotFound, "failed to get user with this username(%s) with error %v", req.User.Username, err)
 	}
 
-	userGrpcToModels(req.User, &user, false)
+	userGrpcToModels(req.User, &user)
 
 	if err := server.DB.Save(&user).Error; err != nil {
 		return nil, status.Errorf(codes.Unknown, "failed to save user updates with error %v.", err)
 	}
 	var updatedUser v1.User
-	userModelsToGrpc(&user, &updatedUser, true)
+	userModelsToGrpc(&user, &updatedUser)
 	return &v1.UpdateUserResponse{
 		User: &updatedUser,
 	}, nil
@@ -57,14 +56,14 @@ func (server UsermanServer) UpdateUser(ctx context.Context, req *v1.UpdateUserRe
 
 func (server UsermanServer) DeleteUser(ctx context.Context, req *v1.DeleteUserRequest) (*v1.DeleteUserResponse, error) {
 	var user models.User
-	if err := server.DB.First(&user, req.Uid).Error; err != nil {
-		return nil, status.Errorf(codes.NotFound, "failed to get user with this id(%d) with error %v", req.Uid, err)
+	if err := server.DB.First(&user, req.Username).Error; err != nil {
+		return nil, status.Errorf(codes.NotFound, "failed to get user with this username(%s) with error %v", req.Username, err)
 	}
 	if err := server.DB.Delete(&user).Error; err != nil {
-		return nil, status.Errorf(codes.Unknown, "failed to delete user with this id(%d) with error %v", req.Uid, err)
+		return nil, status.Errorf(codes.Unknown, "failed to delete user with this user(%s) with error %v", req.Username, err)
 	}
 	var deletedUser v1.User
-	userModelsToGrpc(&user, &deletedUser, true)
+	userModelsToGrpc(&user, &deletedUser)
 	return &v1.DeleteUserResponse{
 		User: &deletedUser,
 	}, nil
